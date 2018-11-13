0558ce216371c43e3184e804be9073b4f093d1ed
Felix <fhidalgo2065@gmail.com>
2018-11-13 07:33:49 -0500

    end to end of finding places
    - type in your address
    - get near by places names

---
+++ functions/src/index.ts
@@ -0,0 +1,71 @@
+import * as functions from 'firebase-functions';
+import * as httpClient from 'request';
+type Response = functions.Response;
+
+const config = functions.config();
+
+function enableCORS(response: Response): void {
+    response.set('Access-Control-Allow-Origin', '*');
+}
+
+function handleOptionsCORS(response: Response): void {
+    response.set('Access-Control-Allow-Methods', 'GET');
+    response.set('Access-Control-Allow-Headers', 'Content-Type');
+    response.set('Access-Control-Max-Age', '3600');
+    response.status(204).send('');
+}
+
+export const coordinates = functions.https.onRequest((request, response) => {
+
+    enableCORS(response);
+
+    if (request.method === 'OPTIONS') {
+        handleOptionsCORS(response);
+    } else {
+        httpClient.get(
+            { url: `https://maps.googleapis.com/maps/api/geocode/json?address=${request.query.address}&key=${config.googleservice.apikey}` },
+            function (error, res, body) {
+                if (!error && res.statusCode === 200) {
+                    response.send(body);
+                }
+            });
+    }
+});
+
+export const places = functions.https.onRequest((request, response) => {
+
+    enableCORS(response);
+
+    if (request.method === 'OPTIONS') {
+        handleOptionsCORS(response);
+    } else {
+        console.log("@@ in else");
+        httpClient.get(
+            { 
+                url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${request.query.latitude},${request.query.longitude}&radius=1500&type=restaurant&key=${config.googleservice.apikey}` },
+            function (error, res, body) {
+                if (!error && res.statusCode === 200) {
+                    response.send(body);
+                }
+            });
+    }
+});
+
+export const map = functions.https.onRequest((request, response) => {
+
+    enableCORS(response);
+
+    if (request.method === 'OPTIONS') {
+        handleOptionsCORS(response);
+    } else {
+        httpClient.get(
+            { url: `https://maps.googleapis.com/maps/api/staticmap?center=${request.query.address}&zoom=13&size=600x300&maptype=roadmap&markers=color:red%7C${request.query.address}&key=${config.googleservice.apikey}` },
+            function (error, res, body) {
+                if (!error && res.statusCode === 200) {
+                    //response.contentType("image/png");
+                    response.sendfile
+                    response.send(body);
+                }
+            });
+    }
+});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,22 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+
+interface Coordinates {
+  lat: number;
+  lng: number;
+}
+interface Location {
+  location: Coordinates;
+}
+interface Result {
+  geometry: Location;
+  name: String;
+}
+interface MyResponse {
+  results: Array<Result>;
+}
 
 @Component({
   selector: 'app-root',
@@ -7,4 +25,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'where-to-go';
+  address: String = '';
+  binaryImage: any;
+  displayImage = false;
+  places: Array<Result>;
+
+  constructor(private http: HttpClient, private sanitizer: DomSanitizer) {
+  }
+
+  search() {
+    this.http.get(`http://localhost:5000/where-to-go-2acea/us-central1/coordinates?address=${this.address}`)
+      .subscribe(value => {
+        const latitude = (value as MyResponse).results[0].geometry.location.lat;
+        const longitude = (value as MyResponse).results[0].geometry.location.lng;
+        console.log(latitude);
+        console.log(longitude);
+
+        this.http.get(`http://localhost:5000/where-to-go-2acea/us-central1/places?latitude=${latitude}&longitude=${longitude}`)
+          .subscribe(places => {
+            this.places = (places as MyResponse).results;
+          });
+
+      });
+  }
 }
