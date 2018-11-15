ce192f733db24fa1cadb8d461f13168c49a15818
Felix <fhidalgo2065@gmail.com>
2018-11-15 06:16:22 -0500

    Clean up deployment
    - made functions build apart of travis ci not the deployment
    - made functions deployment send seperate slack message

--- messageSlack.sh
+++ messageSlack.sh
@@ -1,1 +1,1 @@
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Just deployed to Firebase Hosting"}' https://hooks.slack.com/services/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
+curl -X POST -H 'Content-type: application/json' --data '{"text":"$1"}' https://hooks.slack.com/services/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
