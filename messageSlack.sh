500d1f7ab40d5b45b5b52c52097c743dbdd71089
Felix <fhidalgo2065@gmail.com>
2019-01-27 16:51:20 -0500

    changing the deployment message

--- messageSlack.sh
+++ messageSlack.sh
@@ -1,1 +1,2 @@
-curl -X POST -H 'Content-type: application/json' --data '{"text":"Just deployed to Firebase"}' https://hooks.slack.com/services/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
+#!/bin/bash
+curl -X POST -H 'Content-type: application/json' --data '{"text": "'"$1"'"}' https://hooks.slack.com/services/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
