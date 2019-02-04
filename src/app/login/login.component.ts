22f0896df69776d3b838724cf6479d632c1e1afe
Joe King <kingje@vcu.edu>
2019-02-04 18:38:46 -0500

    Got redirects working with FirebaseUI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -14,7 +15,7 @@ export class LoginComponent implements OnInit {
 
 
 
-  constructor(public authserv: AuthService) { }
+  constructor(public authserv: AuthService, public router: Router) { }
 
   signUpWithEmailAndPassword() {
     if (this.txtEmail != null && this.txtPassword != null) {
@@ -33,6 +34,13 @@ export class LoginComponent implements OnInit {
   //     this.txtEnterCredentials = 'Please input email and password';
   //   }
   // }
+  goHome() {
+    this.router.navigateByUrl('/home');
+  }
+
+  failedLogin() {
+    console.log('Login Credentials Did Not Pass.');
+  }
 
   ngOnInit() {
   }
